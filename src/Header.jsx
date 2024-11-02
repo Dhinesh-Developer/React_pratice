a26a06ccdc6166e4ed49e1d869535bf2402efb7f
DHINESHKUMAR M <dhineshdeveloper07@gmail.com>
2024-11-02 12:10:56 +0530

    Add files via upload

---
+++ src/Header.jsx
@@ -0,0 +1,20 @@
+function Header(){
+    return(
+        <header>
+            <h1>My webiste</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">about</a></li>
+                    <li>
+                    <a href="#">services</a>
+                    </li>
+                    <li><a href="#">Contact</a></li>
+                    
+                </ul>
+                <hr />
+            </nav>
+        </header>
+    );
+}
+export default Header
